8f80ace6b81203af19f20ac096cd489bed9731fd
degrand <nate.prewitt@gmail.com>
2014-09-16 16:56:02 -0600

    added basic setup.py file

---
+++ setup.py
@@ -0,0 +1,19 @@
+import os
+from setuptools import setup
+
+setup(
+    name = "HTTPRequests",
+    version = "0.0.1",
+    author = "Nate Prewitt",
+    description = "Basic HTTP request library",
+    license = "BSD",
+    keywords = "request http",
+    url = "http://github.com/degrand/HTTPRequests",
+    packages = ['HTTPRequests'],
+    classifiers = ["Development Status :: 2 - Pre-Alpha",
+                   "Environment :: Console",
+                   "Programming Language :: Python :: 2.7",
+                   "Topic :: Internet :: WWW/HTTP",
+                   "Licesnse :: OSI Approved :: BSD License"
+    ],
+)
